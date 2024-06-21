f34094db58f6dcc067f67f03ae4e9e13f1a6a9ef
TungTT <tungtran962003@gmail.com>
2024-06-21 18:32:48 +0700

    first commit

---
+++ src/main/java/com/example/movie_web_be/MovieWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_web_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieWebBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movie_web_be/MovieWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie_web_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWebBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
